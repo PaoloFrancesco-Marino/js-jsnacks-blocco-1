a86a99976e7f2254cf937192b23e1f147e210191
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-03 15:40:42 +0200

    snack 2 + snack 3 (start snack 4)

--- main.js
+++ main.js
@@ -1,31 +1,112 @@
-/**
+/**************************************************************************
  * Snack 1: L’utente inserisce due parole in successione, con due prompt.
  *  Il software stampa la parola più lunga. 
  * Utilizza una funzione per determinare e ritornare la parola più lunga.
- */
+ **************************************************************************/
 
- var parola1 = prompt('inserisci parola 1');
- var parola2 = prompt('inserisci parola 2');
+//  var parola1 = prompt('inserisci parola 1');
+//  var parola2 = prompt('inserisci parola 2');
 
- console.log(parola1);
- console.log(parola2);
+//  console.log(parola1);
+//  console.log(parola2);
 
- var risultato = wordlength(parola1, parola2);
- console.log(risultato);
+//  var risultato = wordlength(parola1, parola2);
+//  console.log(risultato);
  
 
-//  function Snack 1
+// //  function Snack 1
 
-function wordlength (word1, word2) {
+// function wordlength (word1, word2) {
 
-    if (word1.length > word2.length) {
-        return 'la parola 1 è più lunga: ' + word1;
+//     if (word1.length > word2.length) {
+//         return 'la parola 1 è più lunga: ' + word1;
 
-    } else if (word1.length < word2.length) {
-        return 'la parola 2 è più lunga: ' + word2;
-    } 
+//     } else if (word1.length < word2.length) {
+//         return 'la parola 2 è più lunga: ' + word2;
+//     } 
 
-    return 'le parole hanno la stessa lunghezza'
-}
+//     return 'le parole hanno la stessa lunghezza'
+// }
+
+
+/*****************************************************
+ * Snack 2: Inserisci un numero, 
+ * se è pari stampa il numero, 
+ * se è dispari stampa il numero successivo
+ *****************************************************/
+
+//  var numUtente = parseInt(prompt('inserisci un numero'));
+//  console.log(numUtente);
  
+
+// //  if (numUtente % 2 == 0) {
+// //      console.log('numero pari ' + numUtente);
+     
+// //  } else {
+// //      console.log('numero dispari stampo il successivo ' + ++numUtente);
+     
+// //  }
+
+// //  alternativa con function 
+
+// var risultato = isPari(numUtente);
+// console.log(risultato);
+
+// function isPari (numero) {
+//     if (numero % 2 == 0) {
+//         return 'il numero è pari';
+//     }
+
+//     return 'il numero è dispari stampo il successivo ' + ++numero;
+// }
  
+
+ /*****************************************************************
+  * Snack 3: Generatore di “nomi cognomi” casuali: 
+  * prendendo una lista di nomi (‘Michele’, ‘Fabio’, ‘Roberto’) 
+  * e una lista di cognomi (‘Forghieri’, ‘Papagni’, ‘Marazzini’), 
+  * Gatsby vuole generare una falsa lista di 5 invitati.
+  *****************************************************************/
+ 
+
+
+
+// var surnames = ["Forghieri", "Papagni", "Marazzini"];
+
+// var names = ["Michele", "Fabio", "Roberto"];
+
+// var guestsList = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var randomIndexName =  getRandomNumber(0, names.length)
+//     var randomIndexSurname = getRandomNumber(0, surnames.length)
+    
+//     var newName = names[randomIndexName] + ' ' + surnames[randomIndexSurname];
+//     guestsList.push(newName);
+// }
+
+// console.log(guestsList);
+
+// // function 
+
+// function getRandomNumber (min, max) { 
+//     var random = Math.floor(Math.random() * (max - min) ) + min;
+//     return random; 
+// }
+
+
+/*********************************************************************
+ * Snack 4: Crea un array di numeri interi, 
+ * ad esempio (1, 4, 2, 15, 120, 17, 3) 
+ * e fai la somma di tutti gli elementi che sono in posizione dispari. 
+ * Prova prima senza funzione e poi con funzione.
+ *********************************************************************/
+
+ var numInt = [1, 4, 2, 15, 120, 17, 3];
+
+ for (var i = 0; i < numInt.length; i++) {
+     var item = numInt[i];
+     console.log(item);
+          
+ }
+
